8815ca42282e572eef420cfcef89e9d00a069a9c
kumkumthakur2411 <157138629+kumkumthakur2411@users.noreply.github.com>
2025-01-12 00:45:55 -0800

    Update Header.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import styles from './Header.module.css';
 import { CiMenuKebab } from "react-icons/ci";
+import { IoCloseOutline } from "react-icons/io5";
 const Header = () => {
     const [isMenuOpen, setIsMenuOpen] = useState(false);
     const toggleMenu = () => {
@@ -12,6 +13,17 @@ const Header = () => {
         <h1 className={styles.logo}>Neecop</h1>
         <div className={styles.nav1}>
             <div className={styles.nav2 }>
+                <a href="#">home</a>
+                <a href="#">about</a>
+                <a href="#">product</a>
+                <a href="#">content</a>
+                <a href="#">service</a>
+                <span></span>            
+            </div>
+            <div 
+             className={`${styles.slidebar} ${isMenuOpen ? styles.open : ""}`}>
+            <IoCloseOutline className={styles.closeIcon}
+                            onClick={toggleMenu} />
                 <a href="#">home</a>
                 <a href="#">about</a>
                 <a href="#">product</a>
